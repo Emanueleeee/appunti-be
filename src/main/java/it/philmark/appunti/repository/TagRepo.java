790414d5219b28723799f5223e58643f2899cd32
Desmocris <cristian.cirulli.93@gmail.com>
2022-07-21 15:26:42 +0200

    TagRepo

---
+++ src/main/java/it/philmark/appunti/repository/TagRepo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package it.philmark.appunti.repository;
+
+import it.philmark.appunti.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author crist
+ */
+public interface TagRepo extends JpaRepository<Tag, Long>
+{
+    
+}
