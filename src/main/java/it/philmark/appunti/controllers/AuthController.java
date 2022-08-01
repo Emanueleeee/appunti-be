921d9fe729baef19ecef2f3a22d8ee1147543c02
Emanueleeee <lelex.ea.ea@gmail.com>
2022-08-01 09:42:27 +0200

    push nome

--- src/main/java/it/philmark/appunti/controllers/AuthController.java
+++ src/main/java/it/philmark/appunti/controllers/AuthController.java
@@ -35,7 +35,7 @@ import it.philmark.appunti.security.service.UserDetailsImpl;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
-public class OutController {
+public class AuthController {
 	@Autowired
 	AuthenticationManager authenticationManager;
 
